ea5d50645b9b3f7bb0596357dfdbe7549bb3b89a
katherineam97 <39163032+katherineam97@users.noreply.github.com>
2022-03-06 16:38:02 -0600

    Create findCommomAndDifferentNumbersBetweenArrays.java
    1) Given 2 arrays find the numbers in common
    2) Given 2 arrays find the different numbers

---
+++ java/findCommomAndDifferentNumbersBetweenArrays.java
@@ -0,0 +1,70 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* 1) Given 2 arrays find the numbers in common
+   2) Given 2 arrays find the different numbers */
+class Exercise
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    int[] firstArray = { 2, 4, 1, 8};
+	    int[] secondArray = { 7, 2, 5, 8};
+	    
+		Exercise ex = new Exercise();
+		System.out.println("Common");
+		ex.findCommonNumbers(firstArray, secondArray);
+		System.out.println("Different");
+		ex.findDifferentNumbers(firstArray, secondArray);
+	}
+	
+	public void findCommonNumbers(int[] firstArray, int[]secondArray){
+	    HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+	    for(int index = 0; index < firstArray.length; ++index){
+	        if(map.get(firstArray[index]) == null){
+	            map.put(firstArray[index], 1);
+	        }else{
+	            map.put(firstArray[index], map.get(firstArray[index]) + 1);
+	        }
+	    }
+	    
+	    for(int index = 0; index < secondArray.length; ++index){
+	        if(map.get(secondArray[index]) == null){
+	            map.put(secondArray[index], 1);
+	        }else{
+	            map.put(secondArray[index], map.get(secondArray[index]) + 1);
+	        }
+	    }
+	    
+	   for (Integer number : map.keySet()) { // obtain all the keys in hashmap
+	       if(map.get(number) > 1)
+                System.out.println(number);
+        }
+	}
+	
+	public void findDifferentNumbers(int[] firstArray, int[]secondArray){
+	    	    HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+	    for(int index = 0; index < firstArray.length; ++index){
+	        if(map.get(firstArray[index]) == null){
+	            map.put(firstArray[index], 1);
+	        }else{
+	            map.put(firstArray[index], map.get(firstArray[index]) + 1);
+	        }
+	    }
+	    
+	    for(int index = 0; index < secondArray.length; ++index){
+	        if(map.get(secondArray[index]) == null){
+	            map.put(secondArray[index], 1);
+	        }else{
+	            map.put(secondArray[index], map.get(secondArray[index]) + 1);
+	        }
+	    }
+	    
+	   for (Integer number : map.keySet()) { // obtain all the keys in hashmap
+	       if(map.get(number) == 1)
+                System.out.println(number);
+        }
+	}
+}
